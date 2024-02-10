cf36c8b41487e1775b0281810ab39b5163981151
Javid <javid.momeni@gmail.com>
2024-02-10 19:34:14 +0330

    re build

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Grid from '@mui/material/Unstable_Grid2';
 import Box from '@mui/material/Box';
 import { Card } from '@mui/material';
 
+
 WebApp.expand();
 
   const MainButton = WebApp.MainButton;
--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,35 @@ import { clarity } from 'react-microsoft-clarity';
 
 import WebApp from '@twa-dev/sdk'
 import eruda from 'eruda'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import rtlPlugin from 'stylis-plugin-rtl';
 
+
+
+// Create an RTL cache
+const cacheRtl = createCache({
+  key: 'muirtl',
+  stylisPlugins: [rtlPlugin],
+});
+
+const theme = createTheme({
+  typography: {
+    fontFamily: '"VazirMatn", serif',
+  },
+  direction: 'rtl',
+});
 eruda.init()
 WebApp.ready();
 clarity.init('kzhxxy2ip7');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <CacheProvider value={cacheRtl}>
+<ThemeProvider theme={theme}>
+<React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </ThemeProvider>
+  </CacheProvider>
 )
