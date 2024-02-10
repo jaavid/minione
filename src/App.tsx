2aa9f9ec2258bf1c3023491d8404d4d293282820
Javid <javid.momeni@gmail.com>
2024-02-10 16:35:43 +0330

    re build

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,16 @@
 import { useState, useEffect } from 'react';
 import './App.css'
 import WebApp from '@twa-dev/sdk'
+  const MainButton = WebApp.MainButton;
+  const BackButton = WebApp.BackButton;
 
-// function App() {
-//   const [count, setCount] = useState(0);
-//   const userdata = WebApp.initDataUnsafe;
-//   console.log(userdata);
-//   console.log(userdata.user?.id);
-//   console.log(WebApp);
-
-//   const MainButton = WebApp.MainButton;
-//   const BackButton = WebApp.BackButton;
-
-//   MainButton.setText('بازی');
-//   MainButton.show();
-//   MainButton.onClick(() => alert('submitted'));
-//   MainButton.color = '#000000';
-//   MainButton.textColor = '#FFFFFF';
-//   BackButton.show();
-//   BackButton.onClick(() => window.history.back());
-
-//   return (
-//     <>
-//       <h1>Elemental Game</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//       </div>
-//         {/* Here we add our button with alert callback */}
-//       <div className="card">
-//         <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-//             Show Alert
-//         </button>
-//         <button onClick={() => WebApp.showAlert(` ${userdata}`)}>
-//           Show User Data
-//         </button>
-//       </div>
-//     </>
-//   )
-// }
-
-// export default App
+  MainButton.setText('بازی');
+  MainButton.show();
+  MainButton.onClick(() => alert('submitted'));
+  MainButton.color = '#000000';
+  MainButton.textColor = '#FFFFFF';
+  BackButton.show();
+  BackButton.onClick(() => window.history.back());
 
 interface ScoreData {
   userid: number;
