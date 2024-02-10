a90e0be47c919a8b03c77fc7544b37d6a1ff8941
Javid <javid.momeni@gmail.com>
2024-02-10 11:59:38 +0330

    build on docs directory

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { clarity } from 'react-microsoft-clarity';
 
 import WebApp from '@twa-dev/sdk'
 
 WebApp.ready();
+clarity.init('kzhxxy2ip7');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
